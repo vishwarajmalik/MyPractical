f263c70a8f864762b8bbcd7edf4606d83f2fc7e1
Vishwaraj Malik <vishwaraj.malik@gmail.com>
2023-08-02 04:19:29 +0530

    add some Algos

---
+++ Algos/CLS_Algos.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyAlgos
+{
+    class CLS_Algos
+    {
+        public void Init(string[] args)
+        {
+
+            string input = "Fri 05:00-10:00\n";
+
+             int[] A = {6,1,4,6,3,2,7,4 };
+
+            solution2(A, 3, 2);
+        }
+
+        // calculate sleep time
+
+        public int solution2(int[] A, int K, int L)
+        {
+
+            // Implement your solution here
+
+            Int32 maxSelect = 0;
+            if ( (K + L )> A.Length)
+            {
+                maxSelect = - 1;
+                return maxSelect;
+            }
+            else if ((K + L) == A.Length)
+            {
+                maxSelect = A.Sum();
+                return maxSelect;
+            }
+
+            Dictionary<int, bool> dictTreeSelected = new Dictionary<int, bool>();
+
+            int noOfTrees = A.Length;
+            
+            List<Int32> lstTotalApples = new List<Int32>();
+            lstTotalApples.Add(0);
+
+            for (int i = 0; i < noOfTrees; i++)
+            {
+                lstTotalApples.Add(lstTotalApples[i] + A[i]);
+            }
+
+            for (int i = 0; i <= noOfTrees - K; i++)
+            {
+                int person1 = lstTotalApples[i + K] - lstTotalApples[i];
+
+                for (int j = 0; j <= noOfTrees - L; j++)
+                {
+                    if (j >= i + K || j + L <= i)
+                    {
+                        int person2 = lstTotalApples[j + L] - lstTotalApples[j];
+                        maxSelect = Math.Max(maxSelect, person1 + person2);
+                    }
+                }
+
+                dictTreeSelected[i] = true;
+            }
+
+            return maxSelect;
+        }
+
+
+        public int MaxApples(int[] A, int K, int L)
+        {
+            int n = A.Length;
+            if (K + L > n) return -1;
+
+
+
+            // Calculate the prefix sum of apples for efficient sliding window computation
+
+            int maxApples = 0;
+            List<Int32> lst = new List<Int32>();
+            lst.Add(0);
+
+            for (int i = 0; i < n; i++)
+            {               
+                lst.Add(lst[i] + A[i]);
+            }
+           
+            for (int i = 0; i <= n - K; i++)
+            {
+                int person1 = lst[i + K] - lst[i];
+               
+                for (int j = 0; j <= n - L; j++)
+                {
+                    if (j >= i + K || j + L <= i)
+                    {
+                        int person2 = lst[j + L] - lst[j];
+                        maxApples = Math.Max(maxApples, person1 + person2);
+                    }
+                }
+            }
+
+            return maxApples;
+        }
+
+
+        static int GetMaxApples(int[] apples)
+        {
+            int n = apples.Length;
+            int maxApples = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int aliceApples = 0;
+                int bobApples = 0;
+
+                // Calculate apples for Alice
+                for (int j = i; j < n; j += 2)
+                {
+                    aliceApples += apples[j];
+                }
+
+                // Calculate apples for Bob
+                for (int j = i + 1; j < n; j += 2)
+                {
+                    bobApples += apples[j];
+                }
+
+                // Update maxApples if necessary
+                maxApples = Math.Max(maxApples, Math.Max(aliceApples, bobApples));
+            }
+
+            return maxApples;
+        }
+
+
+        static int CalculateMaxApples(int[] apples, bool preferAlice)
+        {
+            int totalApples = 0;
+            foreach (int apple in apples)
+                totalApples += apple;
+
+            int eachPersonGets = totalApples / 2;
+            int remainingApples = totalApples % 2;
+
+            if (preferAlice)
+                return eachPersonGets + remainingApples;
+            else
+                return eachPersonGets;
+        }
+
+        public int Sleep_Solution(string S)
+        {
+
+            // Implement your solution here
+
+            Int32 sleepDuration = 0;
+
+            string[] arrinput = S.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            List<SlotBE> listSlot = new List<SlotBE>();
+
+            DateTime minDate = DateTime.MinValue;
+
+            foreach (var slot in arrinput)
+            {
+
+                Console.WriteLine($"slot row---> {slot}");
+                try
+                {
+                    string[] parts = slot.Split(' ');
+                    int noOfday = GetDayNoOfWeek(parts[0]);
+                    string[] timeParts = parts[1].Split('-');
+                    TimeSpan startTime = ParseTime(timeParts[0]);
+                    TimeSpan endTime = ParseTime(timeParts[1]);
+
+                    DateTime startDateTime = minDate.AddDays(noOfday).Add(startTime);
+                    DateTime endDateTime = minDate.AddDays(noOfday).Add(endTime);
+
+                    SlotBE slotBE = new SlotBE
+                    {
+                        StartTime = startDateTime,
+                        EndTime = endDateTime,
+                        SlotRow = slot
+                    };
+                    listSlot.Add(slotBE);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("error for " + slot + System.Environment.NewLine);
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+
+            var listSlotSorted = listSlot.OrderBy(slot => slot.StartTime);
+
+            DateTime prevEndTime = DateTime.MinValue;
+            DateTime workEndTime = DateTime.MinValue.AddDays(6).Add(ParseTime("24:00"));
+
+            foreach (SlotBE slot in listSlotSorted)
+            {
+                //Console.WriteLine($"chekcing for SlotRow {slot.SlotRow}");
+
+                if (prevEndTime != DateTime.MinValue)
+                {
+                    TimeSpan gap = slot.StartTime - prevEndTime;
+
+                    if (gap.TotalMinutes > sleepDuration)
+                    {
+                        sleepDuration = Convert.ToInt32(gap.TotalMinutes);
+                        //Console.WriteLine($"sleepDuration -->  {sleepDuration} for slot {slot.SlotRow}");
+                    }
+                }
+                else
+                {
+                    TimeSpan gap = slot.StartTime - prevEndTime;
+                    if (gap.TotalMinutes > sleepDuration)
+                    {
+                        sleepDuration = Convert.ToInt32(gap.TotalMinutes);
+                        //Console.WriteLine($"sleepDuration -->  {sleepDuration} for slot {slot.SlotRow}");
+                    }
+                }
+                prevEndTime = slot.EndTime;
+            }
+            TimeSpan ts = workEndTime - prevEndTime;
+
+            if (ts.TotalMinutes > sleepDuration)
+            {
+                sleepDuration = Convert.ToInt32(ts.TotalMinutes);
+
+            }
+
+            Console.WriteLine($"sleepDuration --> {sleepDuration}" );
+
+            return sleepDuration;
+        }
+
+        static TimeSpan ParseTime(string timeString)
+        {
+            if (timeString == "24:00")
+                return TimeSpan.FromHours(24);
+
+            return TimeSpan.Parse(timeString);
+        }
+
+        public class SlotBE
+        {
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+            public string SlotRow { get; set; }           
+        }
+
+        static int GetDayNoOfWeek(string day)
+        {
+            switch (day.ToLower())
+            {
+                case "sun":
+                    return 6;
+                case "mon":
+                    return 0;
+                case "tue":
+                    return 1;
+                case "wed":
+                    return 2;
+                case "thu":
+                    return 3;
+                case "fri":
+                    return 4;
+                case "sat":
+                    return 5;
+                default:
+                    throw new ArgumentException("Invalid day of the week.");
+            }
+        }
+
+        public static int test_solution(int[] A)
+        {
+
+            // Implement your solution here
+            Int32 op = 1;
+            List<Int32> lst = A.Distinct().ToList();
+            lst.Sort();
+
+            for (int i = 0; i < lst.Count; i++)
+            {
+                if (lst[i] != i + 1)
+                {
+                    op = i + 1;
+                    break;
+                }
+            }
+            Console.WriteLine(op);
+
+            return op;
+
+        }
+    }
+}
---
+++ Algos/WordSearch.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace WordSearch
+{
+	// word search in 8 direction
+    class Program
+    {
+        static char[,] Grid = new char[,]{
+            {'C', 'P', 'K', 'X', 'O', 'I', 'G', 'H', 'S', 'F', 'C', 'H'},
+            {'Y', 'G', 'W', 'R', 'I', 'A', 'H', 'C', 'Q', 'R', 'X', 'K'},
+            {'M', 'A', 'X', 'I', 'M', 'I', 'Z', 'A', 'T', 'I', 'O', 'N'},
+            {'E', 'T', 'W', 'Z', 'N', 'L', 'W', 'G', 'E', 'D', 'Y', 'W'},
+            {'M', 'C', 'L', 'E', 'L', 'D', 'N', 'V', 'L', 'G', 'P', 'T'},
+            {'O', 'J', 'A', 'A', 'V', 'I', 'O', 'T', 'E', 'E', 'P', 'X'},
+            {'C', 'D', 'B', 'P', 'H', 'I', 'A', 'W', 'V', 'X', 'U', 'I'},
+            {'L', 'G', 'O', 'S', 'S', 'B', 'R', 'Q', 'I', 'A', 'P', 'K'},
+            {'E', 'O', 'I', 'G', 'L', 'P', 'S', 'D', 'S', 'F', 'W', 'P'},
+            {'W', 'F', 'K', 'E', 'G', 'O', 'L', 'F', 'I', 'F', 'R', 'S'},
+            {'O', 'T', 'R', 'U', 'O', 'C', 'D', 'O', 'O', 'F', 'T', 'P'},
+            {'C', 'A', 'R', 'P', 'E', 'T', 'R', 'W', 'N', 'G', 'V', 'Z' }
+        };
+
+        static string[] Words = new string[]
+        {
+            "CARPET",
+            "CHAIR",
+            "DOG",
+            "BALL",
+            "DRIVEWAY",
+            "FISHING",
+            "FOODCOURT",
+            "FRIDGE",
+            "GOLF",
+            "MAXIMIZATION",
+            "PUPPY",
+            "SPACE",
+            "TABLE",
+            "TELEVISION",
+            "WELCOME",
+            "WINDOW"
+        };
+
+        static Dictionary<char, List<CharPosition>> dictCharPosition = new Dictionary<char, List<CharPosition>>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Word Search");
+
+            List<CharPosition> lstPos;
+
+            for (int y = 0; y < 12; y++)
+            {
+                for (int x = 0; x < 12; x++)
+                {
+                    Console.Write(Grid[y, x]);
+
+                    char current = Grid[y, x];
+
+                    // keep all all postions of all characters in Dictionary to that we can get  the 1st letter postion for each word.
+                    // so that we dont need tostart from [0,0] for all words.
+
+                    CharPosition pos = new CharPosition
+                    {
+                        ColNo = x,
+                        RowNo = y
+                    };
+
+                    if (dictCharPosition.ContainsKey(current))
+                    {
+                        lstPos = dictCharPosition[current];
+                    }
+                    else
+                    {
+                        lstPos = new List<CharPosition>();
+                    }
+                    lstPos.Add(pos);
+
+
+                    dictCharPosition[current] = lstPos;
+
+
+                    Console.Write(' ');
+                }
+                Console.WriteLine("");
+
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Found Words");
+            Console.WriteLine("------------------------------");
+
+            FindWords();
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to end");
+            Console.ReadKey();
+        }
+
+        private static void FindWords()
+        {
+            //Find each of the words in the grid, outputting the start and end location of
+            //each word, e.g.
+            //PUPPY found at (10,7) to (10, 3);
+
+            PrintCoordinates("GO1LF");
+            //PrintCoordinates("PUPPY");
+            //PrintCoordinates("LABO");
+
+            Console.WriteLine("------------------------------");
+
+            foreach (string word in Words)
+            {
+                PrintCoordinates(word);
+            }
+        }
+
+        private static void PrintCoordinates(string word)
+        {
+            bool found = false;
+            char[] chars = word.ToCharArray();
+            int X, Y;
+
+            int noOfRows = Grid.GetLength(0);
+            int noOfCols = Grid.GetLength(1);
+            List<CharPosition> firstLetterPos = new List<CharPosition>();
+            if (dictCharPosition.ContainsKey(chars[0]))
+            {
+                firstLetterPos = dictCharPosition[chars[0]];
+            }
+
+            // check if all letters of word exists in dictCharPosition
+            foreach (var c in chars)
+            {
+                if (!dictCharPosition.ContainsKey(c))
+                {
+                    Console.WriteLine($"{word} not found.");
+                    return;
+                }
+            }             
+
+            // Console.WriteLine($"{word} first letter match count is {firstLetterPos.Count}");
+
+            List<string> lstMatchePos = new List<string>();
+            for (int j = 0; j < firstLetterPos.Count; j++)
+            {
+                int colNo = firstLetterPos[j].ColNo;
+                int rowNo = firstLetterPos[j].RowNo;
+
+                // check left to right
+                if ((noOfCols - colNo) >= chars.Length)
+                {
+                    lstMatchePos = MatchWord(chars, colNo, rowNo, SearchDirection.LeftToRight);
+                }
+
+                // check top to bottom
+                if (lstMatchePos.Count == 0 && (noOfRows - rowNo) >= chars.Length)
+                {
+                    lstMatchePos = MatchWord(chars, colNo, rowNo, SearchDirection.TopToBottom);
+                }
+
+                // check right to left
+                if (lstMatchePos.Count == 0 && (colNo + 1 >= chars.Length))
+                {
+                    lstMatchePos = MatchWordReverse(chars, colNo, rowNo, SearchDirection.RightToLeft);
+                }
+
+                // check bottom to top
+                if (lstMatchePos.Count == 0 && (1 + rowNo >= chars.Length))
+                {
+                    lstMatchePos = MatchWordReverse(chars, colNo, rowNo, SearchDirection.BottomToTop);
+                }
+
+                // check Diagonally 
+
+                // check UpperLeft Diagonally rowno-1, colNo-1
+                if (lstMatchePos.Count == 0 && (colNo + 1 >= chars.Length && rowNo + 1 >= chars.Length))
+                {
+                    lstMatchePos = MatchWordDiagonally(chars, colNo, rowNo, SearchDirection.UpperLeft);
+                }
+
+                //// check UpperRight Diagonally
+                if (lstMatchePos.Count == 0 && (rowNo + 1 >= chars.Length && (noOfCols - colNo) >= chars.Length))
+                {
+                    lstMatchePos = MatchWordDiagonally(chars, colNo, rowNo, SearchDirection.UpperRight);
+                }
+
+                //// check BottomLeft Diagonally
+                if (lstMatchePos.Count == 0 && (colNo + 1 >= chars.Length && (noOfRows - rowNo) >= chars.Length))
+                {
+                    lstMatchePos = MatchWordDiagonally(chars, colNo, rowNo, SearchDirection.BottomLeft);
+                }
+
+                // check BottomRight Diagonally
+                if (lstMatchePos.Count == 0 && ((noOfCols - colNo) >= chars.Length) && (noOfRows - rowNo) >= chars.Length)
+                {
+                    lstMatchePos = MatchWordDiagonally(chars, colNo, rowNo, SearchDirection.BottomRight);
+                }
+
+                if (lstMatchePos.Count == chars.Length)
+                {
+                    Console.WriteLine($"{word} found at {lstMatchePos.First()} to {lstMatchePos.Last()}");
+                    break;
+                }
+            }
+
+            if (lstMatchePos.Count == 0)
+            {
+                Console.WriteLine($"{word} not found");
+            }
+        }
+
+        enum SearchDirection
+        {
+            LeftToRight = 1,
+            TopToBottom = 2,
+            RightToLeft = 3,
+            BottomToTop = 4,
+            UpperLeft = 5,
+            UpperRight = 6,
+            BottomLeft = 7,
+            BottomRight = 8
+        }
+
+        private static List<string> MatchWord(char[] chars, int colNo, int rowNo, SearchDirection searchDirection)
+        {
+            bool isMatch = false;
+            List<string> matchedPos = new List<string>();
+            matchedPos.Add($" ({colNo}, {rowNo})");
+
+            for (int i = 1; i < chars.Length; i++)
+            {
+                try
+                {
+                    if (searchDirection == SearchDirection.LeftToRight && chars[i] == Grid[rowNo, colNo + i])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo + i}, {rowNo})");
+                    }
+                    else if (searchDirection == SearchDirection.TopToBottom && chars[i] == Grid[rowNo + i, colNo])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo}, {rowNo + i})");
+                    }
+                    else
+                    {
+                        isMatch = false;
+                    }
+
+                    if (!isMatch)
+                    {
+                        matchedPos.Clear();
+                        break;
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"MatchWord {ex.Message} and rowno --> {rowNo}, colNo --> {colNo} i --> ${i}");
+                }
+            }
+            return matchedPos;
+        }
+
+        private static List<string> MatchWordReverse(char[] chars, int colNo, int rowNo, SearchDirection searchDirection)
+        {
+            bool isMatch = false;
+            List<string> matchedPos = new List<string>();
+            matchedPos.Add($"({colNo}, {rowNo})");
+
+            for (int i = 1; i < chars.Length; i++)
+            {
+                try
+                {
+                    if (searchDirection == SearchDirection.RightToLeft && chars[i] == Grid[rowNo, colNo - i])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo - i}, {rowNo})");
+                    }
+                    else if (searchDirection == SearchDirection.BottomToTop && chars[i] == Grid[rowNo - i, colNo])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo}, {rowNo - i})");
+                    }
+                    else
+                    {
+                        isMatch = false;
+                    }
+                    if (!isMatch)
+                    {
+                        matchedPos.Clear();
+                        break;
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"MatchWordReverse {ex.Message} and rowno --> {rowNo}, colNo --> {colNo} i --> ${i}");
+                }
+            }
+            return matchedPos;
+        }
+
+        private static List<string> MatchWordDiagonally(char[] chars, int colNo, int rowNo, SearchDirection searchDirection)
+        {
+            bool isMatch = false;
+            List<string> matchedPos = new List<string>
+            {
+                $"({colNo}, {rowNo})"
+            };
+            
+            for (int i = 1; i < chars.Length; i++)
+            {
+                try
+                {
+                    if (searchDirection == SearchDirection.UpperLeft && chars[i] == Grid[rowNo - i, colNo - i])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo - i}, {rowNo - i})");
+                    }
+                    else if (searchDirection == SearchDirection.UpperRight && chars[i] == Grid[rowNo - i, colNo + i])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo + i}, {rowNo - i})");
+                    }
+                    else if (searchDirection == SearchDirection.BottomRight && chars[i] == Grid[rowNo + i, colNo + i])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo + i}, {rowNo + i})");
+                    }
+                    else if (searchDirection == SearchDirection.BottomLeft && chars[i] == Grid[rowNo + i, colNo - i])
+                    {
+                        isMatch = true;
+                        matchedPos.Add($"({colNo - i}, {rowNo + i})");
+                    }
+                    else
+                    {
+                        isMatch = false;
+                    }
+                    if (!isMatch)
+                    {
+                        matchedPos.Clear();
+                        break;
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"MatchWordDiagonally {ex.Message} and rowno --> {rowNo}, colNo --> {colNo} i --> ${i}");
+                }
+            }
+            return matchedPos;
+        }
+    }
+
+    public class CharPosition
+    {
+        public int ColNo { get; set; }
+        public int RowNo { get; set; }
+    }
+}
